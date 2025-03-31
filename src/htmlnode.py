f0d80f8d71e5d2cefb16f6e6adb838f3d24e981c
red <s_ellis@outlook.com>
2025-03-31 09:36:28 -0500

    added LeafNode class to HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,24 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode('{self.tag!r}', '{self.value!r}', {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, props)
+        self.value = value
+
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Leaf node must have a value")
+
+        if not self.tag:
+            return self.value
+
+        html = f"<{self.tag}"
+
+        if self.props:
+            for key, value in self.props.items():
+                html += f' {key}="{value}"'
+
+        html += f">{self.value}</{self.tag}>"
--- src/test_html.py
+++ src/test_html.py
@@ -1,4 +1,5 @@
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 
 def test_props_to_html_empty():
@@ -23,6 +24,10 @@ def test_props_to_html_multiple_props():
     assert ' target="_blank"' in result
     # Make sure the length is correct (no extra characters)
     assert len(result) == len(' href="https://www.google.com" target="_blank"')
+
+def test_leaf_to_html_p(self):
+    node = LeafNode("p", "Hello, world!")
+    self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
 if __name__ == "__main__":
     # Run tests
