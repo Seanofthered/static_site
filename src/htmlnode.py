c34a79c936aa1ed7ab8950abf59aa9283c0d2609
red <s_ellis@outlook.com>
2025-04-03 21:47:05 -0500

    created text_node_to_html function in textnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -40,3 +40,17 @@ class LeafNode(HTMLNode):
                 html += f' {key}="{value}"'
 
         html += f">{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError ("Invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,10 @@
+
 from enum import Enum
+from typing import Text
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "normal"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -18,7 +21,23 @@ class TextNode:
         return (self.text == other.text and
                 self.text_type == other.text_type and
                 self.url == other.url)
-    
+
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
-    
+
+def text_node_to_html_node(text_node):
+    match(text_node.type):
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text, None)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text, None)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text, None)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text, None)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href":text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src":text_node.url, "alt" : text_node.text})
+        case _:
+            raise Exception("NOT a valid TextNode type")
